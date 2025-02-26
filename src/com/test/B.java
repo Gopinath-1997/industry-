0db6cdb3ae989b790a1491cb70c9beac37a8de93
Gopinath-1997 <gopinath9708@gmail.com>
2025-02-26 13:50:41 +0530

    few methods add

--- src/com/test/B.java
+++ src/com/test/B.java
@@ -1,15 +1,26 @@
 package com.test;
 
 public class B extends A {
- public void savings() {
-	 System.out.println("welcom greenstechnology vanthingana sothadivanguvinga");
-	 
-		
+	public void savings() {
+		System.out.println("welcom greenstechnology vanthingana sothadivanguvinga");
 	}
-public static void main(String[] args) {
-	B b = new B();
-	b.office();
-	b.savings();
-	
-}
+
+	public void officeBranch() {
+		System.out.println("Avadi");
+	}
+
+	public void officeBranch2() {
+		System.out.println("OMR");
+	}
+
+	public void officeBranch3() {
+		System.out.println("ECR");
+	}
+
+	public static void main(String[] args) {
+		B b = new B();
+		b.office();
+		b.savings();
+
+	}
 }
