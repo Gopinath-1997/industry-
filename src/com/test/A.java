2cef65b37b8f6d9db209df39db5390b2307b74a0
Gopinath-1997 <gopinath9708@gmail.com>
2025-02-26 12:55:52 +0530

    gopi

---
+++ src/com/test/A.java
@@ -0,0 +1,8 @@
+package com.test;
+
+public class A {
+	public void office() {
+		System.out.println("welcome to office");
+	}
+
+}
---
+++ src/com/test/B.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class B extends A {
+ public void savings() {
+	 System.out.println("welcom greenstechnology vanthingana sothadivanguvinga");
+	 
+		
+	}
+public static void main(String[] args) {
+	B b = new B();
+	b.office();
+	b.savings();
+	
+}
+}
